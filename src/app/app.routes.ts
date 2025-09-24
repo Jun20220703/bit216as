e4930e2773680fd0f4084937159d21aafcab6fdd
Jun20220703 <kkjhhyu0405@gmail.com>
2025-09-25 03:01:12 +0800

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { InventoryComponent } from './components/browseFoodInventory/browse-inventory.component';
 import { RegistrationPageComponent } from './components/registerAndPrivacySettings/registrationPage/registrationPage.component';
-
+import { LoginPageComponent } from './components/registerAndPrivacySettings/loginPage/loginPage.component';
 
 export const routes: Routes = [
   { path: 'inventory', component: InventoryComponent },
   { path: 'registration', component: RegistrationPageComponent },
-  { path: '', redirectTo: 'registration', pathMatch: 'full' }
+  { path: '', redirectTo: 'registration', pathMatch: 'full' },
+  { path: 'login', component: LoginPageComponent },
+  { path: '', redirectTo: 'login', pathMatch: 'full' }
 ];
