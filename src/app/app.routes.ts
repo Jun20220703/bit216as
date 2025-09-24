d11a185142f665a0d3453bbe7b6327f9885c6065
Kaikaikaiz <yingxin0411@gmail.com>
2025-09-24 22:26:02 +0800

    route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,7 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes } from '@angular/router';
 import { InventoryComponent } from './components/browseFoodInventory/browse-inventory.component';
 
-
-const routes: Routes = [
-  { path: 'inventory', component: InventoryComponent },  
-  { path: '', redirectTo: 'inventory', pathMatch: 'full' } 
+export const routes: Routes = [
+  { path: 'inventory', component: InventoryComponent },
+  { path: '', redirectTo: 'inventory', pathMatch: 'full' }
 ];
-
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
-export class AppRoutingModule {}
