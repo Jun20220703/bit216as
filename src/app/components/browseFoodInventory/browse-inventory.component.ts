a0ab8d144a998b6463f483f1e5355debd1c0aa0e
Kaikaikaiz <yingxin0411@gmail.com>
2025-09-24 11:07:55 +0800

    test

--- src/app/components/browseFoodInventory/browse-inventory.component.ts
+++ src/app/components/browseFoodInventory/browse-inventory.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common'; 
+import { FormsModule } from '@angular/forms';    
 
 @Component({
   selector: 'app-inventory',
+  standalone: true,
   templateUrl: './browse-inventory.component.html',
-  styleUrls: ['./browse-inventory.component.css']
+  styleUrls: ['./browse-inventory.component.css'],
+  imports: [CommonModule, FormsModule]  
 })
+
 export class InventoryComponent {
   locations = ['Fridge', 'Freezer', 'Shelf'];
   selectedLocation = 'Fridge';
