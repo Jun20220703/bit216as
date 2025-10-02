2f65d3b6bcaeda0d7bfbb00070215b9e6286f92b
ANyangpa <171238641+ANyangpa@users.noreply.github.com>
2025-10-02 18:24:21 +0800

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { FoodInventoryComponent } from './components/manageFoodInventory/manage-inventory.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'inventory', pathMatch: 'full' }, 
+    { path:'inventory', component: FoodInventoryComponent}
+
+];
