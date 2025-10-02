1fc6962fd712f651d41f47c09677b10c028942ed
ANyangpa <171238641+ANyangpa@users.noreply.github.com>
2025-10-02 19:42:35 +0800

    Update manage-inventory.component.ts

--- src/app/components/manageFoodInventory/manage-inventory.component.ts
+++ src/app/components/manageFoodInventory/manage-inventory.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SidebarComponent } from "../sidebar/sidebar.component";
 
 @Component({
   selector: 'app-food-inventory',
   templateUrl: './manage-inventory.component.html',
-  styleUrls: ['./manage-inventory.component.css']
+  styleUrls: ['./manage-inventory.component.css'],
+  imports: [SidebarComponent]
 })
 export class ManageFoodInventory {
   foodItems = [
