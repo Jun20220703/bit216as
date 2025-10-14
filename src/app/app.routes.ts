31ce799a40c3e4f2ce7b8204e040c9b6b496f5a3
Jun20220703 <kkjhhyu0405@gmail.com>
2025-10-14 09:32:09 +0800

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -20,6 +20,5 @@ export const routes: Routes = [
   { path: 'add-food', component: AddFoodItemComponent},
   { path: 'donation-list', component: DonationListComponent},
   { path: 'verification', component: VerificationComponent},
-  { path: '', redirectTo: 'home', pathMatch: 'full' }
   { path: '', redirectTo: 'login', pathMatch: 'full' }
 ];
